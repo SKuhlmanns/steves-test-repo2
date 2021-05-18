ddf584806ff874357b4bc8213f230b0c67e85e1d
Steven Kuhlmann <Steven.Kuhlmann@nov.com>
2021-05-18 18:01:29 -0500

    Change how github artifacts are downloaded

--- build/get_github_artifacts.sh
+++ build/get_github_artifacts.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-# Use this script to download and unzip github artifacts by run d.
+# Use this script to download and unzip github artifacts by run id and user entered
+# artifact list.
 
-EXPECTED_ARGS=2
+EXPECTED_ARGS=3
 E_BADARGS=65
 
 if [ $# -lt $EXPECTED_ARGS ]
 then
     clear
-    echo "Usage: $0 {run_id} {password_token} '[artifact_list]'"
+    echo "Usage: $0 {run_id} {password_token} '{artifact_list}'"
     echo "Where: {run_id} = ########"
     echo "       {password_token} = Users Github password or token"
-    echo "       [artifact_list] = optional space separated list of quoated artifacts"
+    echo "       {artifact_list} = a file containing and artifact list or"
+    echo "                         a space separated list of quoated artifacts"
     echo
     echo "Examples:"
     echo
-    echo "$0 832176399 b133llcd44faf86966567f3gb3c23733943a3456"
+    echo "$0 832176399 b133llcd44faf86966567f3gb3c23733943a3456 lib_artifact.lst"
     echo "$0 832176399 b133llcd44faf86966567f3gb3c23733943a3456 'version-num- version-'"
     exit $E_BADARGS
 fi
@@ -25,33 +27,29 @@ password_token=${2}
 artifact_list=${3}
 build_dir=`dirname $0`
 
-# Check to see if a artifact_list has been entered by the user.
-# If not, use the github artifacts list in the build directory.
-if [ -z "${artifact_list}" ]; then
-    if [ -f ${build_dir}/github_artifact.lst ]; then 
-        artifact_list=`cat ${build_dir}/github_artifact.lst`
-    else
-	   echo
-       echo "Error - You must enter some artifacts to download"
-       echo "        or add a ${build_dir}/github_artifact.lst file."
-       exit 1
-    fi
+# Check to see if a artifact file has been entered by the user.
+# If not, use the user entered artifact list.
+artifact_file=`echo ${artifact_list} | cut -d" " -f1`
+if [ -f "${build_dir}/${artifact_file}" ]; then
+    artifact_list=`cat ${build_dir}/${artifact_file}`
 fi
 
-# Use the run id to get a list of artifacts to download from github.
-rm -f artifacts.lst
+# Use the run id and curl to get a list of artifacts that can be downloaded from github.
+rm -f run_id_artifact.lst
 curl -H "Authorization: token ${password_token}" -sL \
 https://api.github.com/repos/SKuhlmanns/steves-test-repo/actions/runs/${run_id}/artifacts \
 -o artifacts.txt
 
-# Strip out the name and id from the curl output; generate an artifact list file.
+# Strip out the name and id from the curl output; generate a run id artifact list file.
 cat artifacts.txt | grep '"name":' | cut -d'"' -f4 > name.txt
 cat artifacts.txt | grep '"id":' | cut -d':' -f2 | cut -d"," -f1 > id.txt
-paste -d "" name.txt id.txt > artifacts.lst
+paste -d "" name.txt id.txt > run_id_artifact.lst
 rm id.txt name.txt artifacts.txt
-cat artifacts.lst
+#cat run_id_artifact.lst
 
-# Use the artifact list file and to download artifact zip files.
+# Compare the user enter artifact list against the generated run id artifact list.
+# Download zipped artifacts by artifact id that are in both lists, where *"${artifact_name}"*
+# is contained in run id artifacts list 
 for artifact_name in ${artifact_list}
 do
     while IFS= read -r line; do
@@ -64,7 +62,7 @@ do
 https://api.github.com/repos/SKuhlmanns/steves-test-repo/actions/artifacts/${artifact_id}/zip
             fi
         fi
-    done < artifacts.lst
+    done < run_id_artifact.lst
 done
 
 # Unzip and delete the downloaded zip files.
@@ -74,7 +72,7 @@ while IFS= read -r line; do
         unzip -u ${filename}.zip
         rm ${filename}.zip
     fi
-done < artifacts.lst
+done < run_id_artifact.lst
 
-# Delete the generated artifacts list file.
-rm -f artifacts.lst
+# Delete the generated run id artifacts list file.
+rm -f run_id_artifact.lst
